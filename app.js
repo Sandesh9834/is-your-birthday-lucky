fff39862eebb1099eec709faf2d392636a9c793a
Sandesh9834 <78275234+Sandesh9834@users.noreply.github.com>
2021-08-28 11:41:54 +0530

    date(object) to number conversion

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ console.log("hello");
 var birthDate = document.querySelector("#date");
 var luckyNumber = document.querySelector("#number");
 var bthCheck = document.querySelector("#check")
+var sum = 0;
+var strConcat = "";
 
 function enteredDate()
 {
@@ -11,8 +13,19 @@ function enteredDate()
       // console.log(strDate);
       // console.log("Type of strDate is " + typeof(strDate))
       var array = strDate.split("-");
-      console.log(array);
+      // console.log(array);
+      // console.log(array[0]);
+      // console.log(array[1]);
+      // console.log(array[2]);
 
+      for(var i = 0; i<array.length; i++)
+      {
+            strConcat += array[i];
+            strConcat.split("0/")
+      }
+      dateNum = Number(strConcat)
+      console.log(strConcat)
+      
       // console.log("Entered date is: " + birthDate.value);
       // // console.log("Entered number is: " + luckyNumber.value);
 
